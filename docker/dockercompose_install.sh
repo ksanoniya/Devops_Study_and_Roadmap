5489e933dc11a7a4af51c13930e7dd0ea37a596f
kalash <kalashsanoniya1818@gmail.com>
2025-07-18 20:26:12 +0530

    docker or docker add some  commands

--- docker/dockercompose_install.sh
+++ docker/dockercompose_install.sh
@@ -15,3 +15,14 @@ sudo apt-get install docker-compose-plugin -y
 
 #check docker compose plugin version
 docker compose version
+
+
+# compose run command
+docker compose up -d
+
+# check if docker compose is running
+docker compose ps
+
+# delete docker compose
+docker compose down
+
--- docker/install_and_configuration.sh
+++ docker/install_and_configuration.sh
@@ -17,8 +17,17 @@ docker run hello-world
 
 # docker build and run commands
 docker build -t hello-world .
-docker run hello-world --name hello
--world  
+docker run hello-world
 
 # docker run with options
-    
+docker run -d -p 80:80 --name webserver nginx
+
+# check running containers
+docker ps
+
+# stop and remove containers
+docker stop webserver
+docker rm webserver
+
+# remove docker image
+docker rmi nginx    
