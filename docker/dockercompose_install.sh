e5a88785ba94ca5b3775e4cd53910ae80eb4c407
kalash <kalashsanoniya1818@gmail.com>
2025-07-18 09:11:56 +0530

    create and write docker-compose file

---
+++ docker/dockercompose_install.sh
@@ -0,0 +1,17 @@
+#update linux
+sudo apt-get update -y
+
+#upgrade linux
+sudo apt-get upgrade -y
+
+#install docker compose
+sudo apt-get install docker-compose -y
+
+#check docker compose version
+docker-compose --version
+
+#install docker compose plugin
+sudo apt-get install docker-compose-plugin -y
+
+#check docker compose plugin version
+docker compose version
