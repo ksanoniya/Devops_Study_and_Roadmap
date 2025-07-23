0fedcabe2f50d026c6bc6720183bbf5665295316
kalash <kalashsanoniya1818@gmail.com>
2025-07-23 18:45:46 +0530

    Add kubernetes kind

---
+++ kubernetes/kind/install_command.sh
@@ -0,0 +1,31 @@
+#linux
+# For AMD64 / x86_64
+sudo apt-get update -y
+sudo apt-get install -y curl
+# For ARM64
+sudo apt-get install -y curl
+# install kind
+# For AMD64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+# check kind version
+kind version
+
+#macos
+# For Intel Macs
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-darwin-amd64
+# For M1 / ARM Macs
+[ $(uname -m) = arm64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-darwin-arm64
+chmod +x ./kind
+mv ./kind /some-dir-in-your-PATH/kind
+# check kind version
+kind version
+
+# windows
+curl.exe -Lo kind-windows-amd64.exe https://kind.sigs.k8s.io/dl/v0.29.0/kind-windows-amd64
+Move-Item .\kind-windows-amd64.exe c:\some-dir-in-your-PATH\kind.exe
+# check kind version
+kind version
